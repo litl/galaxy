54a6b3e94b84217586c02e327159d75ebfe2d0ad
Jason Wilder <jwilder@litl.com>
2014-03-17 16:08:27 -0600

    Allow overriding global options w/ env vars
    So we can specify a different etcd host for remote commands

--- galaxy.go
+++ galaxy.go
@@ -433,6 +433,14 @@ func loadConfig() {
 	}
 
 }
+
+func getEnv(name, defaultValue string) string {
+	if os.Getenv(name) == "" {
+		return defaultValue
+	}
+	return os.Getenv(name)
+}
+
 func main() {
 
 	loadConfig()
@@ -446,9 +454,9 @@ func main() {
 	app.Name = "galaxy"
 	app.Usage = "galaxy cli"
 	app.Flags = []cli.Flag{
-		cli.StringFlag{Name: "etcd", Value: "http://127.0.0.1:4001", Usage: "host:port[,host:port,..]"},
-		cli.StringFlag{Name: "env", Value: "dev", Usage: "environment (dev, test, prod, etc.)"},
-		cli.StringFlag{Name: "pool", Value: "web", Usage: "pool (web, worker, etc.)"},
+		cli.StringFlag{Name: "etcd", Value: getEnv("GALAXY_ETCD_HOST", "http://127.0.0.1:4001"), Usage: "host:port[,host:port,..]"},
+		cli.StringFlag{Name: "env", Value: getEnv("GALAXY_ENV", "dev"), Usage: "environment (dev, test, prod, etc.)"},
+		cli.StringFlag{Name: "pool", Value: getEnv("GALAXY_POOL", "web"), Usage: "pool (web, worker, etc.)"},
 	}
 
 	app.Commands = []cli.Command{
