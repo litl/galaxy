e09abd6c8325abe9c215e9ea0cb8b5ca2a837b02
Jason Wilder <jwilder@litl.com>
2014-06-27 16:07:23 -0600

    Use DOCKER_HOST if set

--- runtime/runtime.go
+++ runtime/runtime.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"os/signal"
+	"strconv"
 	"strings"
 	"time"
 
@@ -69,7 +70,74 @@ func GetEndpoint() string {
 
 }
 
+// based off of https://github.com/dotcloud/docker/blob/2a711d16e05b69328f2636f88f8eac035477f7e4/utils/utils.go
+func parseHost(addr string) (string, string, error) {
+	var (
+		proto string
+		host  string
+		port  int
+	)
+	addr = strings.TrimSpace(addr)
+	switch {
+	case addr == "tcp://":
+		return "", "", fmt.Errorf("Invalid bind address format: %s", addr)
+	case strings.HasPrefix(addr, "unix://"):
+		proto = "unix"
+		addr = strings.TrimPrefix(addr, "unix://")
+		if addr == "" {
+			addr = "/var/run/docker.sock"
+		}
+	case strings.HasPrefix(addr, "tcp://"):
+		proto = "tcp"
+		addr = strings.TrimPrefix(addr, "tcp://")
+	case strings.HasPrefix(addr, "fd://"):
+		return "fd", addr, nil
+	case addr == "":
+		proto = "unix"
+		addr = "/var/run/docker.sock"
+	default:
+		if strings.Contains(addr, "://") {
+			return "", "", fmt.Errorf("Invalid bind address protocol: %s", addr)
+		}
+		proto = "tcp"
+	}
+
+	if proto != "unix" && strings.Contains(addr, ":") {
+		hostParts := strings.Split(addr, ":")
+		if len(hostParts) != 2 {
+			return "", "", fmt.Errorf("Invalid bind address format: %s", addr)
+		}
+		if hostParts[0] != "" {
+			host = hostParts[0]
+		} else {
+			host = "127.0.0.1"
+		}
+
+		if p, err := strconv.Atoi(hostParts[1]); err == nil && p != 0 {
+			port = p
+		} else {
+			return "", "", fmt.Errorf("Invalid bind address format: %s", addr)
+		}
+
+	} else if proto == "tcp" && !strings.Contains(addr, ":") {
+		return "", "", fmt.Errorf("Invalid bind address format: %s", addr)
+	} else {
+		host = addr
+	}
+	if proto == "unix" {
+		return proto, host, nil
+
+	}
+	return proto, fmt.Sprintf("%s:%d", host, port), nil
+}
+
 func dockerBridgeIp() (string, error) {
+	dh := os.Getenv("DOCKER_HOST")
+	if dh != "" && strings.HasPrefix(dh, "tcp") {
+		_, hostPort, err := parseHost(dh)
+		return strings.Split(hostPort, ":")[0], err
+	}
+
 	dockerZero, err := net.InterfaceByName("docker0")
 	if err != nil {
 		return "", err
@@ -92,7 +160,7 @@ func (s *ServiceRuntime) ensureDockerClient() *docker.Client {
 		endpoint := GetEndpoint()
 		client, err := docker.NewClient(endpoint)
 		if err != nil {
-			panic(err)
+			log.Fatalf("ERROR: Unable to connect to docker: %s: %s", err, endpoint)
 		}
 		s.dockerClient = client
 
