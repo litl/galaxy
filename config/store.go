ea080b51757b7f73a66b1c5562158f5633dd4d7b
Jason Wilder <jwilder@litl.com>
2014-10-29 10:12:45 -0600

    Move ConfigStore funcs to store file

--- config/store.go
+++ config/store.go
@@ -6,7 +6,6 @@ import (
 	"path"
 	"strings"
 
-	"github.com/litl/galaxy/log"
 	"github.com/litl/galaxy/utils"
 )
 
@@ -266,4 +265,104 @@ func (r *ConfigStore) ListEnvs() ([]string, error) {
 		}
 	}
 	return envs, nil
+}
+
+func (r *ConfigStore) Get(app, env string) (*ServiceConfig, error) {
+	return r.GetServiceConfig(app, env)
+}
+
+func (r *ConfigStore) GetServiceConfig(app, env string) (*ServiceConfig, error) {
+	exists, err := r.AppExists(app, env)
+	if err != nil || !exists {
+		return nil, err
+	}
+
+	svcCfg := NewServiceConfig(path.Base(app), "")
+
+	err = r.LoadVMap(path.Join(env, app, "environment"), svcCfg.environmentVMap)
+	if err != nil {
+		return nil, err
+	}
+	err = r.LoadVMap(path.Join(env, app, "version"), svcCfg.versionVMap)
+	if err != nil {
+		return nil, err
+	}
+
+	err = r.LoadVMap(path.Join(env, app, "ports"), svcCfg.portsVMap)
+	if err != nil {
+		return nil, err
+	}
+
+	return svcCfg, nil
+}
+
+func (r *ConfigStore) SetServiceConfig(svcCfg *ServiceConfig, env string) (bool, error) {
+
+	for k, v := range svcCfg.Env() {
+		if svcCfg.environmentVMap.Get(k) != v {
+			svcCfg.environmentVMap.Set(k, v)
+		}
+	}
+
+	for k, v := range svcCfg.Ports() {
+		if svcCfg.portsVMap.Get(k) != v {
+			svcCfg.portsVMap.Set(k, v)
+		}
+	}
+
+	//TODO: user MULTI/EXEC
+	err := r.SaveVMap(path.Join(env, svcCfg.Name, "environment"),
+		svcCfg.environmentVMap)
+
+	if err != nil {
+		return false, err
+	}
+
+	err = r.SaveVMap(path.Join(env, svcCfg.Name, "version"),
+		svcCfg.versionVMap)
+
+	if err != nil {
+		return false, err
+	}
+
+	err = r.SaveVMap(path.Join(env, svcCfg.Name, "ports"),
+		svcCfg.portsVMap)
+
+	if err != nil {
+		return false, err
+	}
+
+	err = r.notifyChanged(env)
+	if err != nil {
+		return false, err
+	}
+
+	return true, nil
+}
+
+func (r *ConfigStore) DeleteServiceConfig(svcCfg *ServiceConfig, env string) (bool, error) {
+	deletedOne := false
+	deleted, err := r.backend.Delete(path.Join(env, svcCfg.Name))
+	if err != nil {
+		return false, err
+	}
+
+	deletedOne = deletedOne || deleted == 1
+
+	for _, k := range []string{"environment", "version", "ports"} {
+		deleted, err = r.backend.Delete(path.Join(env, svcCfg.Name, k))
+		if err != nil {
+			return false, err
+		}
+		deletedOne = deletedOne || deleted == 1
+	}
+
+	if deletedOne {
+		err = r.notifyChanged(env)
+		if err != nil {
+			return deletedOne, err
+		}
+	}
+
+	return deletedOne, nil
 }
